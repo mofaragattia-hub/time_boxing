fc714e459283a9cabc856f72bf8e7b754e9aea6b
mofaragattia-hub <mo.faragattia@gmail.com>
2025-10-27 15:01:33 +0300

    add admob id

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -9,7 +9,7 @@ class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-9254331228151287/9067595017';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
@@ -19,7 +19,7 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-9254331228151287/3273817638';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/4411468910';
     } else {
